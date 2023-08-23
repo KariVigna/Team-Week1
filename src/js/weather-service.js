e5ee126866f19c754e542305cc9a89a0f223c5ff
Kari Vigna <karivigna24@gmail.com>
2023-08-22 20:05:39 -0700

    project cloned

---
+++ src/js/weather-service.js
@@ -0,0 +1,27 @@
+export default class WeatherService {  
+    static getWeather() {
+      return new Promise(function(resolve, reject) {
+       
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY_2}`;
+            let request = new XMLHttpRequest();
+            request.addEventListener("loadend", function() {
+              const response = JSON.parse(this.responseText);
+              if (this.status === 200) {
+                resolve([response]);
+              } else {
+                reject([this, response]);
+              }
+            });
+            request.open("GET", url, true);
+            request.send();    
+          });
+        } else {
+          reject("Geolocation not supported.")
+        }
+      });
+    }
+  }
